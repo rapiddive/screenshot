4411dea8e1081f66003e3b9d1c172e61c1cb1747
Vinay Shah <vinshah@adobe.com>
2023-03-06 23:43:16 +0530

    commit

--- readme.md
+++ readme.md
@@ -1,2 +1,1 @@
 [![Netlify Status](https://api.netlify.com/api/v1/badges/02315c2f-28dc-426a-9108-1d3b2dab9fd6/deploy-status)](https://app.netlify.com/sites/genuine-snickerdoodle-8ea9b1/deploys)
-1
--- src/api.js
+++ src/api.js
@@ -8,11 +8,36 @@ const app = express();
 const router = express.Router();
 
 // Define a route that responds with a JSON object when a GET request is made to the root path
-router.get("/", (req, res) => {
-  res.json({
-    hello: "hi!"
-  });
+router.get('/', async (req, res) => {
+  let url = 'https://adobe-commerce-tester.freetls.fastly.net/adobe-commerce-tester/';
+  if (req.query.url) {
+      url = req.query.url;
+  }
+  if (!url) {
+      return res.status(400).send('Missing URL parameter');
+  }
+  let screenshot = 'Not yet';
+  (async () => {
+      const browser = await puppeteer.launch({
+          args: ['--no-sandbox'],
+          headless: true
+      });
+      const page = await browser.newPage();
+      await page.goto(url,  {
+          waitUntil: "networkidle0"
+        }).catch((err) => {
+          console.log(err);
+        });
+        page.setViewport({width: 800, height: 400});
+      screenshot = await page.screenshot({ fullPage: true });        
+      await browser.close();   
+      res.set('Content-Type', 'image/png');
+       res.send(screenshot);   
+  })();
+  
 });
+
+
 
 // Use the router to handle requests to the `/.netlify/functions/api` path
 app.use(`/.netlify/functions/api`, router);
